1a753092b9fbe9a58606948b03f0ddfa63c5e8b7
TanojKumar6 <99717433+TanojKumar6@users.noreply.github.com>
2022-02-15 16:08:31 +0530

    working with aggregation function

---
+++ src/main/java/com/example/demo/AggregationsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregationsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregationsSpringApplication.class, args);
+	}
+
+}
